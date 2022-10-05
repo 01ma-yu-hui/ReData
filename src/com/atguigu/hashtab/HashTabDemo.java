9a3c20900276fa796390db4febd4da90425adfd0
myh <2722407149@qq.com>
2022-10-05 21:20:55 +0800

    second version

--- src/com/atguigu/hashtab/HashTabDemo.java
+++ src/com/atguigu/hashtab/HashTabDemo.java
@@ -9,7 +9,7 @@ package com.atguigu.hashtab;
  */
 public class HashTabDemo {
     public static void main(String[] args) {
-        System.out.println("myh);
+        System.out.println("myh");
     }
 }
 
